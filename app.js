66a5fd565b5e8035da0cf4db47297af9df21ccc3
boobalan.ms <boobalan.ms@zettaone.com>
2023-03-04 11:09:53 +0530

    "Angular added in app"

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 app.listen(8010);
 
-app.use('/',(req,res,next)=>{
-  console.log("Hello Express.js");
-  res.send(`<h1>Hello Express</h1>`);
+// app.use('/',(req,res,next)=>{
+//   console.log("Hello Express.js");
+//   res.send(`<h1>Hello Express</h1>`);
+// });
+
+app.use(express.static('./frontend/angular/dist/angular'));
+
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/angular' });
 });
